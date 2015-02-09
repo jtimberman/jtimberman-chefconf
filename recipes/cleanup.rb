47cb303ff6306dc4a21d56a559113899b17dca2d
jtimberman <joshua@chef.io>
2015-02-09 11:27:37 -0700

    host.domain.domain will not work out so well

--- recipes/cleanup.rb
+++ recipes/cleanup.rb
@@ -5,6 +5,7 @@ dnsimple = chef_vault_item('secrets', 'dnsimple')['data']
 %w(chef analytics).each do |host|
 
   dnsimple_record "#{host}.#{dnsimple['domain']}" do
+    name host
     username dnsimple['username']
     password dnsimple['password']
     domain dnsimple['domain']
