32e7a52433d0f113433edfe9c1d86f134a9f00ff
jtimberman <joshua@chef.io>
2015-02-09 11:33:48 -0700

    records need a type and lets have a ttl too

--- recipes/cleanup.rb
+++ recipes/cleanup.rb
@@ -6,6 +6,8 @@ dnsimple = chef_vault_item('secrets', 'dnsimple')['data']
 
   dnsimple_record "#{host}.#{dnsimple['domain']}" do
     name host
+    type 'CNAME'
+    ttl 60
     username dnsimple['username']
     password dnsimple['password']
     domain dnsimple['domain']
